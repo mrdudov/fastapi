3c9ed77a7e9704cb6e8489a95695e9d083486767
Murat <mrdudov@gmail.com>
2022-04-14 13:33:42 +0300

    Path parameters

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-import imp
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+students = {
+    1: {
+        "name": "Jhon",
+        "age": 17,
+        "class": "year 12"
+    }
+}
 
 @app.get('/')
 def index():
     return {
         "name": "First Data"
     }
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int =  Path(None, description="The ID of the student", gt=0, lt=3)):
+    """
+    Get student by ID
+    """
+    return students[student_id]
