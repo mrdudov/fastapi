faa0660e8f00166849fc001ccb6099f86e67aaaa
Murat <mrdudov@gmail.com>
2022-04-15 13:24:12 +0300

    post request

--- course_2/main.py
+++ course_2/main.py
@@ -30,3 +30,8 @@ def root():
 @app.get("/api/v1/users/")
 async def fetch_users():
     return db
+
+@app.post("/api/v1/users/")
+async def register_user(user: User):
+    db.append(user)
+    return {"id": user.id}
