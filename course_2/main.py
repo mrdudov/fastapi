7d1fa80ada590be05de4bdbc74a31ccbb57d393a
Murat <mrdudov@gmail.com>
2022-04-15 14:11:38 +0300

    delete user

--- course_2/main.py
+++ course_2/main.py
@@ -1,6 +1,6 @@
 from typing import List
-from uuid import UUID, uuid4
-from fastapi import FastAPI
+from uuid import UUID
+from fastapi import FastAPI, HTTPException
 
 from models import Gender, Role, User
 
@@ -27,11 +27,23 @@ db: List[User] = [
 def root():
     return {"Hello": "World"}
 
-@app.get("/api/v1/users/")
+@app.get("/api/v1/users")
 async def fetch_users():
     return db
 
-@app.post("/api/v1/users/")
+@app.post("/api/v1/users")
 async def register_user(user: User):
     db.append(user)
     return {"id": user.id}
+
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} does not exists"
+    )
